bdf5ff1dce528aa870b90c7152bb65ca0e7c2e57
xiaohuajun <xiaohuajun@hys-inc.cn>
2016-12-15 13:40:22 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,39 @@
 var webpack = require('webpack');
 
 module.exports = {
+  entry: {
+    index: './client/index.js'
+  },
+  output: {
+    path: __dirname + '/static',
+    filename: '[name]-[chunkhash].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.css', '.json']
+  },
+  plugins: [
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false,
+    //   },
+    //   output: {
+    //     comments: false,
+    //   }
+    // })
+  ]
 }
-
